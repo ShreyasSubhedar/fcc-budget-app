b16f0e988e1a3889884ec85808828e45392a059d
Shreyas Subhedar <shreyassubhedar@gmail.com>
2020-09-03 08:56:09 +0000

    Initial commit

---
+++ budget.py
@@ -0,0 +1,28 @@
+class Category:
+  ledger= list()
+  category=""
+
+  def __init__(self, cat): 
+        self.category = cat 
+
+  def create_spend_chart(categories):
+    pass
+
+  def check_funds(amount):
+    pass
+
+
+  def deposit(self,amount, description=""):
+    self.ledger.append({"amount":amount,"description":description})
+
+
+  def withdraw(amount):
+    return True
+
+  # return balanace of budget 
+  def get_balance():
+    pass
+
+
+  def tranfer(amount,category):
+    pass
